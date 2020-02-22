d677a9f4d57d8a6770fda6e80f7e27a0e897fb41
japopich <japopich@hotmail.com>
2020-02-22 12:08:19 -0700

    Added IMU

--- src/src/fusion.hpp
+++ src/src/fusion.hpp
@@ -2,7 +2,6 @@
  * @file fusion.hpp
  * @brief The main header file for the Code Base
  * 
- * 
  */
 
 #ifndef _FUSION_HPP_
@@ -11,8 +10,24 @@
 #include "Arduino.h"
 #include "Eigen.h"
 #include <Eigen/Eigen>
+#include <Adafruit_BNO055.h>
 
-// Main State Estimation Class
+//! IMU Struct
+/*!
+*   This structs holds the BNO055 sample at a point in time to be stored and processed.
+*/
+struct IMUdata {
+    double GYRO[3] = {0,0,0}; // Gyro in [DPS]
+    double LINEAR_ACCEL[3] = {0,0,0}; // Accelerometer in [m/s^2]
+    double GRAVITY_ACCEL[3] = {0,0,0}; // Accelerometer in [mg]
+    double MAG[3] = {0,0,0}; // Magnetometer in [uT]
+    uint32_t t = 0;
+};
+
+//! Main State Estimation Class
+/*!
+*
+*/
 class State
 {
     private:
@@ -24,7 +39,10 @@ class State
     protected:
 };
 
-// Main Kalman Filter Class
+//! Main Kalman Filter Class
+/*!
+*
+*/
 class Kalman
 {
     private:
@@ -34,6 +52,23 @@ class Kalman
         ~Kalman();
 
     protected:
+};
+
+//! BNO055 IMU Class
+/*!
+*   Class to manage the Adafruit BNO055 Absolute Orientation IMU Fusion breakout board
+*/
+class DigitalIMU {
+    private:
+        Adafruit_BNO055 board;
+        sensors_event_t event;
+        imu::Quaternion quat;
+        imu::Vector<3> accel;
+    public:
+        DigitalIMU();
+        DigitalIMU(int32_t sensorID, uint8_t address);
+        bool begin();
+        void sample(IMUdata* data);
 };
 
 #endif
