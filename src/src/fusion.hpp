68c0467b8fc010ddbf332a7be132c2221b377d4b
japopich <japopich@hotmail.com>
2020-02-21 22:23:40 -0700

    Updates

--- src/src/fusion.hpp
+++ src/src/fusion.hpp
@@ -1,9 +1,39 @@
+/**
+ * @file fusion.hpp
+ * @brief The main header file for the Code Base
+ * 
+ * 
+ */
+
 #ifndef _FUSION_HPP_
 #define _FUSION_HPP_
 
 #include "Arduino.h"
 #include "Eigen.h"
-#include <Eigen/Dense>
 #include <Eigen/Eigen>
+
+// Main State Estimation Class
+class State
+{
+    private:
+
+    public:
+        State();
+        ~State();
+
+    protected:
+};
+
+// Main Kalman Filter Class
+class Kalman
+{
+    private:
+
+    public:
+        Kalman();
+        ~Kalman();
+
+    protected:
+};
 
 #endif
