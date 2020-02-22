8934c3baae57473e2991dbb81d32bf635ccedbc4
japopich <japopich@hotmail.com>
2020-02-22 13:26:15 -0700

    Matrices started to be added

--- src/src/fusion.hpp
+++ src/src/fusion.hpp
@@ -23,6 +23,7 @@ struct IMUdata {
     double LINEAR_ACCEL[3] = {0,0,0}; // Accelerometer in [m/s^2]
     double GRAVITY_ACCEL[3] = {0,0,0}; // Accelerometer in [mg]
     double MAG[3] = {0,0,0}; // Magnetometer in [uT]
+    double Temp = 0;
     uint32_t t = 0;
 };
 
@@ -33,25 +34,9 @@ struct IMUdata {
 class State
 {
     private:
-
     public:
         State();
         ~State();
-
-    protected:
-};
-
-//! Main Kalman Filter Class
-/*!
-*
-*/
-class Kalman
-{
-    private:
-
-    public:
-        Kalman();
-        ~Kalman();
 
     protected:
 };
@@ -72,5 +57,31 @@ class DigitalIMU {
         bool begin();
         void sample(IMUdata* data);
 };
+
+/* All lower case variables are vectors || all uppercase are Matrices*/
+namespace matrices
+{
+    // BOTH DISCARDED AFTER FIRST RUN
+    extern Eigen::VectorXd x_0; // Initial State Vector 
+    extern Eigen::MatrixXd P_0; // Initial Process Covariance Matrix
+
+    // VECTORS
+    extern Eigen::VectorXd x_m; /* Observation Vector 9x1 */
+    extern Eigen::VectorXd x_k; /* Final State Vector 21x1 */
+    extern Eigen::VectorXd x_kp; /* State Prediction 21x1 */
+    extern Eigen::VectorXd x_k_1; /* Previous State Vector (K-1) 21x1 */
+    extern Eigen::VectorXd w_k; /* Predicted State Noise 21x1 */
+    extern Eigen::VectorXd z_k; /* Measurement Noise 21x1 */
+
+     // MATRICES
+    extern Eigen::MatrixXd C; /* Observation Transition Matrix*/
+    extern Eigen::MatrixXd A; /* State Transition Matrix */
+    extern Eigen::MatrixXd P_kp; /* Predicted Process Covariance */
+    extern Eigen::MatrixXd P_k_1; /* Previous Predicted Process Covariance */
+    extern Eigen::MatrixXd R; /* Sensor Covariance Matrix */
+
+    // Identity Matrix
+    extern Eigen::MatrixXd I; /* 21x21 Identity Matrix */
+ };
 
 #endif
